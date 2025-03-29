6b0a05e15fea89fc31d699d2a3e0493833fdbeb6
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-03-28 22:55:48 -0400

    added init function

--- src/components/BS.py
+++ src/components/BS.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 class BS:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, z, N_s, N_rf_b, N_T):
+        self.location = np.array([x, y, z]).reshape((1, 3))
+        self.N_s = N_s
+        self.N_rf_b = N_rf_b
+        self.N_T = N_T

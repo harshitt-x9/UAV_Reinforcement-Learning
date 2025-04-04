517b20dcc2e5e80f700cbf6d5b1af6a614773595
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-04-04 16:11:02 -0400

    CriticNetwork is added

---
+++ src/SAC/CriticNetwork.py
@@ -0,0 +1,27 @@
+import os
+import keras
+import tensorflow as tf
+from keras.layers import Dense
+
+class CriticNetwork(keras.Model):
+    def __init__(self, n_actions, fc1_dims=256, fc2_dims=256,
+            name='critic', chkpt_dir='tmp/sac'):
+        super(CriticNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac.tf')
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.q = Dense(1, activation=None)
+
+    def call(self, state, action):
+        action_value = self.fc1(tf.concat([state, action], axis=1))
+        action_value = self.fc2(action_value)
+
+        q = self.q(action_value)
+
+        return q

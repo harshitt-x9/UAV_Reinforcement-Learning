a61cac55930f39ebebc9ffb7dfe5fe7fb5693324
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-04-09 12:18:24 -0400

    CriticNetwork is created

---
+++ src/SAC/SAC_torch/CriticNetwork.py
@@ -0,0 +1,26 @@
+import os
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+
+
+class CriticNetwork(nn.Module):
+    def __init__(self, beta, input_dims, n_actions, fc1_dims=256, fc2_dims=256,
+            name='critic', chkpt_dir='src/models/trajectory'):
+        super(CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = nn.Linear(self.input_dims[0]+n_actions, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
